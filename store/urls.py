0831f6819d45f4727324981ef451ce5e58d2bc10
KirillAnb <it-reso@mail.ru>
2023-07-02 12:31:28 +0300

    практика 1 закончена

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import CartView, ShopView, ProductSingleView
 
+app_name = 'store'
+
 urlpatterns = [
-    path('shop/', ShopView.as_view()),
-    path('cart/', CartView.as_view()),
-    path('product/', ProductSingleView.as_view()),
+    path('shop/', ShopView.as_view(), name = 'shop'),
+    path('cart/', CartView.as_view(), name = 'cart'),
+    path('product/<int:id>/', ProductSingleView.as_view(), name = 'product'),
 ]
