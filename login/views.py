ffd120a71271522cb3c74a4c5badedd016e9407e
KirillAnb <it-reso@mail.ru>
2023-07-04 11:23:35 +0300

    практика 2 часть 2.2 создание и проверки

--- login/views.py
+++ login/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
+from .forms import CustomUserCreationForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 class LoginView(View):
@@ -24,4 +26,24 @@ class LogoutView(View):
         if request.user.is_authenticated:
             logout(request)
         return redirect("store:shop")
+
+class CreateAccountView(View):
+
+
+   def get(self, request):
+       return render(request, "login/create_account.html")
+
+
+   def post(self, request):
+       form = CustomUserCreationForm(data=request.POST)
+       if form.is_valid():
+           username = form.cleaned_data.get('username')
+           email = form.cleaned_data.get('email')
+           password = form.cleaned_data.get('password1')
+           user = User.objects.create_user(username=username, email=email, password=password)
+           user.save()
+           login(request, user)
+           return redirect('store:shop')
+       return render(request, "login/create_account.html",
+              context={'errors': form.errors})
 
