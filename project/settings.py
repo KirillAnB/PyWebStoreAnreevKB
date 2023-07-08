43faaf6bac728ccaeb445f95128238c16fe91f0b
KirillAnb <it-reso@mail.ru>
2023-07-08 13:03:06 +0300

    Настроен для работы на pythonanywhere

--- project/settings.py
+++ project/settings.py
@@ -87,14 +87,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME_PGDB'),
-        'USER': os.getenv('USER_PGDB'),
-        'PASSWORD': os.getenv('PASSWORD_PGDB'),
-        'HOST': os.getenv('HOST_PGDB'),
-        'PORT': os.getenv('PORT_PGDB'),
-}
+        # 'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.getenv('NAME_PGDB'),
+        # 'USER': os.getenv('USER_PGDB'),
+        # 'PASSWORD': os.getenv('PASSWORD_PGDB'),
+        # 'HOST': os.getenv('HOST_PGDB'),
+        # 'PORT': os.getenv('PORT_PGDB'),
+# }
 
 }
 
@@ -139,7 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if 'localhost' in ALLOWED_HOSTS:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
