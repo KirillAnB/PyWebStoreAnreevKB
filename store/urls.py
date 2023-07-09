ad04713ea16be57562c2207d4d6bea97c48fc69c
KirillAnb <it-reso@mail.ru>
2023-07-09 11:14:27 +0300

    Исправлен store/urls

--- store/urls.py
+++ store/urls.py
@@ -11,7 +11,7 @@ router.register(r'wishlist', WishListViewSet)
 app_name = 'store'
 
 urlpatterns = [
-    path('shop/', ShopView.as_view(), name='shop'),
+    path('', ShopView.as_view(), name='shop'),
     path('cart/', CartView.as_view(), name='cart'),
     path('product/<int:id>/', ProductSingleView.as_view(), name='product'),
     path('wishlist', WishlistView.as_view(), name='wishlist'),
